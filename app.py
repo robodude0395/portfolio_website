2336384377cb31f1e070612ad5866852db5f98a1
Omar <robodude20000@gmail.com>
2025-11-30 21:14:52 +0000

    Set up action to deploy to my site server on push

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify, render_template
+"""Backend code to deploy website to my funtastic portfolio!"""
+from flask import Flask, render_template
 from dotenv import load_dotenv
-from os import environ as ENV
 
 app = Flask(__name__)
 
 @app.get('/')
 def visit_page():
+    """Return my portfolio page html so ppl can see it"""
     return render_template('index.html')
 
 if __name__ == '__main__':
     load_dotenv()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=2137)
